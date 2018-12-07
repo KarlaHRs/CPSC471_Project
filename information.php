fead61c4bf1602c83b1b5ccb04bc7510d3d183c0
breesuisui <breesuisui@gmail.com>
2018-12-07 02:30:00 -0700

    Add files via upload

---
+++ information.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cleaning Company</title>
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/styleGuide.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body>
+<div id="container">
+<div id="banner"></div>
+<div class="navBar-wrap">
+  <div class ="navBar">
+    <ul>
+      <li> <a href="index.php" >Home</a> </li>
+      <li> <a href="information.php" >About Us</a>
+
+      </li>
+
+		 <li> <a href="datePick.php" >Bookings</a> </li>
+
+      <li> <a href="contacts.php" >Contact Us</a> </li>
+		<li> <a href="#" >Login</a>
+		<ul>
+			<li> <a href="login.php" >Member Login</a> </li>
+
+			<li> <a href="admin_login.php" >Admin Login</a> </li>
+			</ul></li>
+    </ul>
+
+
+  </div>
+</div>
+
+
+<div id="content">
+  <h1 class="headingBrown">Welcome to Cleaning Company</h1>
+
+  <p>&nbsp;</p>
+
+<p> We will serve you wholeheartedly</p>
+</div>
+<div id="footer">
+  <p> Copyright&copy; 2018.<br>
+
+  <a href="ziyin.zhao@ucalgary.ca" class="a">Contact Webmaster</a> </p></div>
+  </div>
+
+</body>
+
+</html>
