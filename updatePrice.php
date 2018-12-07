d23731f64aea72b22b653b44e617f29383f0967e
breesuisui <breesuisui@gmail.com>
2018-12-07 00:58:15 -0700

    Update updatePrice.php

--- updatePrice.php
+++ updatePrice.php
@@ -1,1 +1,18 @@
 
+<?php
+
+include ('connect.php');
+include ('header.php');
+    $service = 'service';
+	$price = 'price';
+	
+	
+	$query = "UPDATE SERVICE SET price = 'price'
+	         WHERE service = 'service'";
+	$result = mysql_query ($query);
+	
+	if ($result)
+		echo "<p> update successful </p>";
+	else
+		echo "<p> fail to update </p>";
+?>
