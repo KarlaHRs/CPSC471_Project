5ecdbdeb3208511cf184b45ea4f113b07367a0f0
breesuisui <breesuisui@gmail.com>
2018-12-07 00:19:22 -0700

    Update addService.php

--- addService.php
+++ addService.php
@@ -3,3 +3,20 @@
 include ('connect.php');
 include ('header.php');
 
+    $service = $_POST['service'];
+	  $price = $_POST['price'];
+	
+	
+	function addService($service,$price){
+		try{
+			mysqli_query($conn,"INSERT INTO Service(service,price)
+			VALUES('{$service}','{$price}')");
+		}catch (Exception $e){
+			echo $e ->getMessage();
+		}
+		echo "success";
+	}
+	addService($service,$price);
+	?>
+	
+	
