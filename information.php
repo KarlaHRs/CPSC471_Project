f325f0f31a1ed4c7e8873c75fb6d510051d78a52
breesuisui <breesuisui@gmail.com>
2018-12-14 16:48:54 -0700

    Add files via upload

---
+++ information.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+
+<body>
+	<header class = "navigation">
+		<nav>
+			<div>
+				<a href="homePage.php">Home</a>
+				<a href="information.php">About us</a>
+				<a href="#"> Bookings</a>
+				<a href="contacts.php">Contact us</a>
+				<a href="LoginPage.php">Login</a>
+				
+                               
+
+					</li>
+				</ul>
+				<ul>
+
+	</header>
+	
+<article>
+		Welcome!<br><br>
+		
+		Calgary Cleaning Company, established in September 2018, with a registered capital of 10 million yuan, is a house cleaning company, mainly responsible for indoor cleaning.</br>
+
+
+Based on the principle of "customer first, quality first", the company has established and improved the strict quality standard inspection system, which will surely bring you the most intimate service.<br>
+		
+	</article>
+	
+</body>
+</html>
